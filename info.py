cdc343530412575773c6092697f2b2a326adadac
threatthreat <grezy8007@gmail.com>
2025-03-27 08:05:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002572131699'))
 request_channel = environ.get('REQUEST_CHANNEL', '')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002646558937'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', ''))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002566775846'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '5000')

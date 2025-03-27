c562ed77d62e24ce10e308d076827d47a8423fa4
threatthreat <grezy8007@gmail.com>
2025-03-27 08:01:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,7 +58,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002572131699'))
 request_channel = environ.get('REQUEST_CHANNEL', '')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
-UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', ''))
+UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002646558937'))
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', ''))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)

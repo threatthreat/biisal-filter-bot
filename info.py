122a49a8c41e55bee49fe974e3ba0b04fd32b9b8
threatthreat <grezy8007@gmail.com>
2025-04-25 02:00:04 -0700

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://heisenberg:walder1234@cluster0.wkum1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'walderwald')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002418930268'))
 QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/27xBy1r0/file-908.jpg')
 START_IMG = environ.get('START_IMG', 'https://graph.org/file/4dad0cc16f190468454ee.jpg')
